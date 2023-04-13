cc4528c54ad1c9ac7f6bce074d47cea6d0e6a9a7
YeonJun Cho <micayel1219@gmail.com>
2023-04-13 11:31:08 +0900

    git init

---
+++ src/main/java/com/prography1/eruna/ErunaApplication.java
@@ -0,0 +1,13 @@
+package com.prography1.eruna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prography1/eruna/ErunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prography1.eruna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
