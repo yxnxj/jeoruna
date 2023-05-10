f359df068dde920fd1ee662dc4c9ca510d251cba
SY2on <sjk8w7@gmail.com>
2023-05-11 02:11:37 +0900

    Chore: Swagger 연결 및 설정 세팅
    http://localhost:8080/swagger-ui/index.html
    https://eruna.site/swagger-ui/index.html
    
    Resolves: #3

--- src/main/java/com/prography1/eruna/ErunaApplication.java
+++ src/main/java/com/prography1/eruna/ErunaApplication.java
@@ -1,8 +1,14 @@
 package com.prography1.eruna;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://eruna.site",description = "Eruna Server"),
+		@Server(url = "http://localhost:8080",description = "Local server")})
+@EnableJpaAuditing
 @SpringBootApplication
 public class ErunaApplication {
 
