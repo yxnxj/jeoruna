48fc61717ea105ecd6cb6572120d4e84e89c1d57
YeonJun Cho <micayel1219@gmail.com>
2023-05-29 21:04:05 +0900

    Refactor : JdbcCursorItemReader 대신 JpaPagingItemReader 사용하도록
    이유 :
    - 현재 프로젝트에서 jpa를 사용하고 있음
    - 하나씩 이어가며 stream형식으로 data를 가져오는 방식보다 paging으로 한번에 가져오는 방식이 안전성 측면에서 더 효과적임
    
    relatedTo #25

--- src/main/java/com/prography1/eruna/config/BatchConfig.java
+++ src/main/java/com/prography1/eruna/config/BatchConfig.java
@@ -2,11 +2,13 @@ package com.prography1.eruna.config;
 
 import com.prography1.eruna.domain.entity.Alarm;
 import com.prography1.eruna.domain.entity.DayOfWeek;
+import com.prography1.eruna.domain.enums.Week;
 import com.prography1.eruna.domain.repository.AlarmRepository;
 import com.prography1.eruna.util.AlarmItemProcessor;
 import com.prography1.eruna.util.DayOfWeekRowMapper;
 import com.prography1.eruna.util.JobCompletionNotificationListener;
 import com.prography1.eruna.util.NoOpItemWriter;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.quartz.Scheduler;
 import org.slf4j.Logger;
@@ -21,6 +23,8 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,16 +32,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig{
     private final DataSource dataSource;
-
+    private final EntityManagerFactory entityManagerFactory;
     private static final Logger logger = LoggerFactory.getLogger(BatchConfig.class);
 
-    @Bean
+//    @Bean
     public JdbcCursorItemReader<DayOfWeek> reader(AlarmRepository alarmRepository) {
         JdbcCursorItemReader<DayOfWeek> reader = new JdbcCursorItemReader<>();
         reader.setDataSource(dataSource);
@@ -48,6 +56,22 @@ public class BatchConfig{
         reader.setQueryTimeout(10000);
         reader.close();
         return reader;
+    }
+
+    @Bean
+    public JpaPagingItemReader<DayOfWeek> jpaPagingItemReader(){
+        LocalDate localDate = LocalDate.now();
+        String today = localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT_STANDALONE, new Locale("eng")).toUpperCase(Locale.ROOT);
+        HashMap<String, Object> paramValues = new HashMap<>();
+        paramValues.put("today", Week.valueOf(today));
+
+        return new JpaPagingItemReaderBuilder<DayOfWeek>()
+                .name("alarmReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(2)
+                .queryString("select d from DayOfWeek d where d.dayOfWeekId.day = :today")
+                .parameterValues(paramValues)
+                .build();
     }
 
     @Bean
@@ -68,7 +92,8 @@ public class BatchConfig{
     public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager, AlarmRepository alarmRepository, Scheduler scheduler) {
         return new StepBuilder("step", jobRepository)
                 .<DayOfWeek, Alarm> chunk(10, transactionManager)
-                .reader(reader(alarmRepository))
+//                .reader(reader(alarmRepository))
+                .reader(jpaPagingItemReader())
                 .writer(new NoOpItemWriter(scheduler))
                 .processor(alarmItemProcessor(alarmRepository))
                 .allowStartIfComplete(true)
