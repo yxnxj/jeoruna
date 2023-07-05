37c83f3790d80604f6d6de19e808c262e5f8fcf0
YeonJun Cho <micayel1219@gmail.com>
2023-07-05 12:49:49 +0900

    Test : persistence context에 저장된 Alarm entity Refresh
    weekList가 비어있는 상태의 Alarm을 반환하는 형태에서 다시 DB에서 조회하여 전체 데이터 가져오도록

--- src/test/java/com/prography1/eruna/AlarmServiceTest.java
+++ src/test/java/com/prography1/eruna/AlarmServiceTest.java
@@ -13,22 +13,34 @@ import com.prography1.eruna.domain.repository.GroupRepository;
 import com.prography1.eruna.domain.repository.UserRepository;
 import com.prography1.eruna.service.AlarmService;
 import com.prography1.eruna.web.UserResDto;
+import jakarta.persistence.*;
 import org.aspectj.lang.annotation.Before;
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.provider.PersistenceProvider;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.TextStyle;
 import java.util.*;
 
 
-@SpringBootTest
+//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AlarmServiceTest {
-
     @Autowired
     public UserRepository userRepository;
 
@@ -41,8 +53,12 @@ public class AlarmServiceTest {
     @Autowired
     public DayOfWeekRepository dayOfWeekRepository;
 
+    @Autowired
+    public TestEntityManager testEntityManager;
+
+
     @BeforeEach
-    @Transactional
+//    @Transactional
     public void createAlarmRecordsForTest(){
         int size = 100;
         for (int i = 0; i< size; i++){
@@ -68,7 +84,11 @@ public class AlarmServiceTest {
             DayOfWeek dayOfWeek = new DayOfWeek(dayOfWeekId, alarmRepository.save(alarm));
 
             dayOfWeekRepository.save(dayOfWeek);
+//            List<DayOfWeek> days = new ArrayList<>();
+//            days.add(dayOfWeek);
+//            alarm.setWeekList(days);
         }
+
     }
 
     @Test
@@ -80,12 +100,24 @@ public class AlarmServiceTest {
 
 //        List<Alarm> alarms = alarmRepository.findAllOnDay(day);
         List<Alarm> alarms = alarmRepository.findByWeekList_DayOfWeekId_Day(Week.valueOf(day));
+        for (Alarm alarm : alarms){
+            testEntityManager.refresh(testEntityManager.merge(alarm));
+        }
+//        boolean is = Hibernate.isInitialized(alarms);
 
         long end = System.currentTimeMillis();
         System.out.println("----------------------------------");
         System.out.println("Take time: " + (end - start) + "ms");
         System.out.println("found size : " + alarms.size());
-        Alarm randomAlarm = alarms.get(new Random().nextInt(alarms.size()));
+        Alarm randomAlarm = alarms.get(alarms.size()-1);
+//        entityManager.flush();
+//        entityManager.detach(randomAlarm);
+//        entityManager.clear();
+//        entityManager.close();
+
+        Alarm refreshAlarm = alarmRepository.findById(randomAlarm.getId()).get();
+        List<DayOfWeek> days  =dayOfWeekRepository.findAllByAlarm(randomAlarm);
+
 //        List<String> weekList = new ArrayList<>();
 //        randomAlarm.getWeekList().forEach(week-> weekList.add(week.getDayOfWeekId().getDay().name()));
         System.out.println(randomAlarm.getWeekList());
