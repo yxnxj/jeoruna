dc33dde3738666c5d5141385f44ee07614fee747
YeonJun Cho <micayel1219@gmail.com>
2023-07-16 03:17:18 +0900

    Test : job 실행 전 테스트 데이터 생성, 삭제

--- src/test/java/com/prography1/eruna/batch/scheduler/BatchJobLaunchTests.java
+++ src/test/java/com/prography1/eruna/batch/scheduler/BatchJobLaunchTests.java
@@ -2,10 +2,22 @@ package com.prography1.eruna.batch.scheduler;
 
 
 import com.prography1.eruna.config.FCMConfig;
+import com.prography1.eruna.domain.entity.Alarm;
+import com.prography1.eruna.domain.entity.DayOfWeek;
+import com.prography1.eruna.domain.entity.Groups;
+import com.prography1.eruna.domain.entity.User;
+import com.prography1.eruna.domain.enums.AlarmSound;
+import com.prography1.eruna.domain.enums.Role;
+import com.prography1.eruna.domain.enums.Week;
+import com.prography1.eruna.domain.repository.*;
+import jakarta.transaction.Transactional;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.quartz.Scheduler;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
@@ -13,16 +25,32 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
 
 
 @SpringBatchTest
 @SpringJUnitConfig(TestBatchConfig.class)
 @Import({CustomConfig.class, FCMConfig.class})
 @EnableJpaRepositories("com.prography1.eruna.domain.repository")
+//@SpringBootTest
 //@DataJpaTest
+@EnableJpaAuditing
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BatchJobLaunchTests {
     @Autowired
@@ -37,6 +65,21 @@ public class BatchJobLaunchTests {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    Scheduler scheduler;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    GroupRepository groupRepository;
+
+    @Autowired
+    DayOfWeekRepository dayOfWeekRepository;
+
+    @Autowired
+    AlarmRepository alarmRepository;
+
 
     @BeforeEach
     public void setup(@Autowired Job job) {
@@ -46,17 +89,54 @@ public class BatchJobLaunchTests {
         this.jobRepositoryTestUtils.removeJobExecutions();
     }
 
+    public void createAlarmRecordsForTest(){
+        int size = 3;
+        for (int i = 0; i< size; i++){
+            User user = User.builder()
+                    .role(Role.USER)
+                    .uuid(UUID.randomUUID().toString())
+                    .fcmToken(UUID.randomUUID().toString())
+                    .build();
+
+            Groups group = Groups.create(userRepository.save(user));
+
+            Alarm alarm = Alarm.builder()
+                    .alarmTime(LocalTime.now().plusMinutes(5))
+                    .alarmSound(AlarmSound.ALARM_SIU)
+                    .finishDate(LocalDate.now())
+                    .startDate(LocalDate.now())
+                    .groups(groupRepository.save(group))
+                    .build();
+
+            String day = LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.SHORT_STANDALONE, new Locale("eng")).toUpperCase(Locale.ROOT);
+            Week week = Week.valueOf(day);
+            DayOfWeek.DayOfWeekId dayOfWeekId = new DayOfWeek.DayOfWeekId(alarm.getId(), week);
+            DayOfWeek dayOfWeek = new DayOfWeek(dayOfWeekId, alarmRepository.save(alarm));
+
+            dayOfWeekRepository.save(dayOfWeek);
+        }
+    }
+
     @Test
     public void testMyJob() throws Exception {
         // given
         JobParameters jobParameters = this.jobLauncherTestUtils.getUniqueJobParameters();
+        createAlarmRecordsForTest();
 
 
         // when
         JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
 
+
         // then
         Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+
+    @AfterEach
+    void resetTables(){
+        alarmRepository.deleteAll();
+        groupRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
 }
