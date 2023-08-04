017a93907ed5cab33548bbc88b51886861a5a879
조연준 <micayel1219@gmail.com>
2023-08-04 22:59:47 +0900

    Feat : sse send exception 핸들링
    exception이 발생했을 때 emitter와 thread를 삭제한다. 이후 설정된 timeout 시간으로 sse 재연결이 진행된다.

--- src/main/java/com/prography1/eruna/util/SseEmitters.java
+++ src/main/java/com/prography1/eruna/util/SseEmitters.java
@@ -11,6 +11,7 @@ import com.prography1.eruna.service.WakeupService;
 import com.prography1.eruna.web.UserResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -23,6 +24,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Component
 @Slf4j
@@ -33,15 +35,16 @@ public class SseEmitters {
     private final WakeupService wakeupService;
     private final GroupUserRepository groupUserRepository;
     private final GroupRepository groupRepository;
+
     public SseEmitter add(Long groupId, String uuid) {
-        SseEmitter emitter = new SseEmitter(30 * 60L * 1000);
+        SseEmitter emitter = new SseEmitter(10 * 1000L);
         String key = generateKey(groupId, uuid);
 
 ////        this.emitters.add(emitter);
-        if (emitters.containsKey(key)) {
-            return emitters.get(key);
-        }
-        this.emitters.put(key, emitter);
+//        if (emitters.containsKey(key)) {
+//            return emitters.get(key);
+//        }
+//        this.emitters.put(key, emitter);
         log.info("new emitter added: {}", emitter);
         log.info("emitter list size: {}", emitters.size());
 
@@ -56,16 +59,19 @@ public class SseEmitters {
         emitter.onError((c) -> {
             emitter.completeWithError(c.getCause());
         });
+
+        emitters.putIfAbsent(key, emitter);
+
         return emitter;
     }
 
-    public List<UserResDto.WakeupDto> findWakeupInfo(Long groupId){
+    public List<UserResDto.WakeupDto> findWakeupInfo(Long groupId) {
         List<UserResDto.WakeupDto> list = wakeUpCacheRepository.getWakeupDtoList(groupId);
         /**
-        * 캐싱된 데이터가 없으면 DB에서 캐싱과 동시에 그룹 유저들을 찾아 리스트를 반환한다.
-        */
+         * 캐싱된 데이터가 없으면 DB에서 캐싱과 동시에 그룹 유저들을 찾아 리스트를 반환한다.
+         */
 
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             Groups group = groupRepository.findById(groupId).orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_GROUP));
             List<GroupUser> groupUsers = groupUserRepository.findByGroupsForScheduler(group);
             list = wakeUpCacheRepository.createGroupUsersCache(list, groupId, groupUsers);
@@ -74,7 +80,7 @@ public class SseEmitters {
         return list;
     }
 
-    public void sendWakeupInfo(Long groupId, String uuid){
+    public void sendWakeupInfo(Long groupId, String uuid) {
         List<UserResDto.WakeupDto> list = wakeUpCacheRepository.getWakeupDtoList(groupId);
         String key = generateKey(groupId, uuid);
 
@@ -86,7 +92,7 @@ public class SseEmitters {
          * 캐싱된 데이터가 없으면 DB에서 캐싱과 동시에 그룹 유저들을 찾아 리스트를 반환한다.
          */
 
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             Groups group = groupRepository.findById(groupId).orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_GROUP));
             List<GroupUser> groupUsers = groupUserRepository.findByGroupsForScheduler(group);
             list = wakeUpCacheRepository.createGroupUsersCache(list, groupId, groupUsers);
@@ -105,14 +111,14 @@ public class SseEmitters {
 //            log.error("SSE ERROR : " + e.getMessage());
 //            sseEmitter.completeWithError(e.getCause());
 //        }
-        send(sseEmitter, wakeupDtos);
+        send(sseEmitter, list, groupId, uuid);
 
     }
 
-    public void sendWakeupInfo2All(Long groupId){
+    public void sendWakeupInfo2All(Long groupId) {
         List<UserResDto.WakeupDto> wakeupDtoList = wakeUpCacheRepository.getWakeupDtoList(groupId);
 
-        for(UserResDto.WakeupDto wakeupDto : wakeupDtoList){
+        for (UserResDto.WakeupDto wakeupDto : wakeupDtoList) {
             String uuid = wakeupDto.getUuid();
 
             String key = generateKey(groupId, uuid);
@@ -120,7 +126,7 @@ public class SseEmitters {
             /**
              * 아직 기상 하지 않은 유저는 SSE 연결이 되어 있지 않음
              */
-            if(sseEmitter == null) continue;
+            if (sseEmitter == null) continue;
 //            try {
 //                sseEmitter.send(SseEmitter.event()
 //                        .name("wakeupInfo")
@@ -132,11 +138,11 @@ public class SseEmitters {
 //                sseEmitter.completeWithError(e.getCause());
 //            }
             Object[] wakeupDtos = wakeupDtoList.toArray();
-            send(sseEmitter, wakeupDtos);
+            send(sseEmitter, wakeupDtoList, groupId, uuid);
         }
     }
 
-    private void send(SseEmitter sseEmitter, Object[] wakeupDtos){
+    private void send(SseEmitter sseEmitter, List<UserResDto.WakeupDto> wakeupDtos, Long groupId, String uuid) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(() -> {
             try {
@@ -144,15 +150,19 @@ public class SseEmitters {
                         .name("wakeupInfo")
                         .data(wakeupDtos));
                 log.info("SSE SEND!! : " + wakeupDtos);
-            } catch (IOException e) {
+
+//                sseEmitter.complete();
+            } catch (IOException | IllegalStateException e) {
                 log.error("SSE ERROR : " + e.getMessage());
                 sseEmitter.completeWithError(e.getCause());
+                emitters.remove(generateKey(groupId, uuid));
+                executor.shutdown();
             }
         });
         executor.shutdown();
     }
 
-    private String generateKey(Long groupId, String uuid){
+    private String generateKey(Long groupId, String uuid) {
         return "SSE." + groupId + "." + uuid;
     }
 }
